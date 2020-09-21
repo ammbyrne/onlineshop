645427d2d26c4726f860f52c8bfde6388c294678
ammbyrne <andy_m_byrne@yahoo.co.uk>
2020-09-21 17:09:10 +0800

    new stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+
+//import Example from './pages/navbar/navbar.component';
+
+//import notfound from './pages/notfound/notfound.component';
 
 function App() {
   return (
